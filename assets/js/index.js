ed0f8160ecd2ed8fe1f10013986b6612c454f65d
pollbox <zhnh0591@163.com>
2017-04-24 20:40:39 +0800

    文章分享

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,6 +20,31 @@
             $("body").toggleClass("nav-opened nav-closed");
         });
 
+      //   //分享
+      //     var bhead = $(document.head);
+      //     var config = {
+      //      url: location.href,
+      //      site_url: location.origin,
+      //      source: bhead.find('[property="og:site_name"]').attr('content') || document.title,
+      //      title: bhead.find('[property="og:title"]').attr('content') || document.title,
+      //      description: bhead.find('[property="og:description"]').attr('content') || '',
+      //      image: bhead.find('[property="og:image"]').attr('content') || '',
+      //      imageSelector: undefined,
+       //
+      //      weiboKey: '',
+       //
+      //      wechatQrcodeTitle: '微信扫一扫：分享',
+      //      wechatQrcodeHelper: '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>',
+      //      wechatQrcodeSize: 100,
+       //
+      //      mobileSites: [],
+      //      sites: ['weibo','qq','wechat','tencent','douban','qzone'],
+      //      disabled: ['linkedin','diandian','facebook','twitter','google']
+      //     //  initialized: false
+      //  };
+       //
+       $('.social-share').share();
+
     });
 
     // Arctic Scroll by Paul Adam Davis
